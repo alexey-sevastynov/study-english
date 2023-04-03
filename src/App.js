e139beb9d13f7f96c0879c0c15e48c3af0177720
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-04-03 21:46:58 +0300

    added component "info"

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/header/Header";
+import Info from "./components/info/Info";
 
 class App extends React.Component {
   render() {
     return (
       <div className="app dark_mode">
         <Header />
+        <Info />
       </div>
     );
   }
